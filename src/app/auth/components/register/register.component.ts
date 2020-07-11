cd6a4d47ccc71cb0e195ff4ca787a8ebea3cddb5
Vladimir Tambovtsev <tambovcev99@mail.ru>
2020-07-11 15:07:34 +0300

    set up /register page

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,8 +1,27 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
+import {FormGroup, FormBuilder} from '@angular/forms'
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {}
+export class RegisterComponent implements OnInit {
+  form: FormGroup
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initializeForm()
+  }
+
+  initializeForm(): void {
+    this.form = this.fb.group({
+      username: '',
+      email: '',
+      password: ''
+    })
+  }
+
+  onSubmit(): void {}
+}
